4a349a1390cd34ed54a21e635c82107617975462
brobro332 <332bro@naver.com>
2025-04-09 02:31:56 +0900

    프로젝트 : 프로젝트 생성 및 의존성 주입

---
+++ src/main/java/kr/co/samsami_sentinel_server/SamsamiSentinelServerApplication.java
@@ -0,0 +1,13 @@
+package kr.co.samsami_sentinel_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamsamiSentinelServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamsamiSentinelServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/co/samsami_sentinel_server/SamsamiSentinelServerApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.samsami_sentinel_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamsamiSentinelServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
